739e952a988b036c373c66e3ec2d09550d5a58ce
Jihong Lin <jihong860301@gmail.com>
2021-09-16 18:01:30 +0800

    nhentai_overview.py new ability: show pages and tags

--- nhentai_overview.py
+++ nhentai_overview.py
@@ -20,14 +20,21 @@ for s in sel:
     sel2_title = soup2.select("div h2")
     print(sel2_title[0].text)
     sel2_img = soup2.select("div a img")
-    print(sel2_img[1]["src"])
+    sel_sixnum = soup2.select("div h3")
 
     pic = requests.get(sel2_img[1]["src"])
     img2 = pic.content
     fn = s["href"].split('/')
-    pic_out = open(fn[2]+'.png', 'wb')
+    pic_out = open('#'+fn[2]+'.png', 'wb')
     pic_out.write(img2)
     pic_out.close()
-    list_text.write(fn[2]+':'+sel2_title[0].text+'\n')
+    list_text.write(sel_sixnum[0].text+':'+sel2_title[0].text+'\ttag:')
+    sel2_tags = soup2.select('div section span a')
+    for s in sel2_tags:
+        ss = s['href'].split('/')
+        if ss[1] == 'tag':
+            list_text.write(ss[2]+'\t')
+    list_text.write(sel2_tags[-1].text+'p\n')
+    print(sel_sixnum[0].text, sel2_tags[-1].text+'p')
 
 list_text.close()
