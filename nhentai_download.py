54a86cad983a1a25d5282f6358bda070a82bf25f
Jihong Lin <jihong860301@gmail.com>
2021-09-17 15:00:59 +0800

    make nhentai_download.py can produce pdf

--- nhentai_download.py
+++ nhentai_download.py
@@ -1,8 +1,8 @@
-from genericpath import samefile
 import requests as rq
 from bs4 import BeautifulSoup as bs
 import sys
 import os
+import img2pdf
 
 ##########################################
 six_num = sys.argv[:]
@@ -24,6 +24,7 @@ for sn in six_num[1:]:
     print('total pages', sel[-1].text)
 
     ii = int(sel[-1].text)
+    img_list = []
     for i in range(ii):
         rr = rq.get(nref+str(i+1)+'/')
         soup = bs(rr.text, 'html.parser')
@@ -31,7 +32,9 @@ for sn in six_num[1:]:
         img_src = sel[0]["src"]
         img_get = rq.get(img_src)
         img = img_get.content
-        pic_out = open('./'+sn+'/_'+str(i+1)+'.png', 'wb')
-        pic_out.write(img)
-        pic_out.close()
+        with open('./'+sn+'/_'+str(i+1)+'.png', 'wb') as pic_out:
+            pic_out.write(img)
+        img_list.append('./'+sn+'/_'+str(i+1)+'.png')
         print('Page '+str(i+1)+' done!')
+    with open('./'+sn+'/'+sn+'.pdf', 'wb') as f:
+        f.write(img2pdf.convert(img_list))
