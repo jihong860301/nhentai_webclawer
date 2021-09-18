8a275c4a6fc7ff601f8034cb8190370d4c6643f2
Jihong Lin <jihong860301@gmail.com>
2021-09-18 15:41:56 +0800

    error of multiworks not fixed

--- nhentai_download.py
+++ nhentai_download.py
@@ -3,10 +3,20 @@ from bs4 import BeautifulSoup as bs
 import sys
 import os
 import img2pdf
+from concurrent.futures import ThreadPoolExecutor
 
 ##########################################
 six_num = sys.argv[:]
 ##########################################
+
+
+def get_image(img_src, sn):
+    img = rq.get(img_src, stream=True).content
+    i = img_src.split('/')[5].split('.')[0]
+    print(f'./{sn}/_{i}.png')
+    with open(f'./{sn}/_{i}.png', 'wb') as pic_out:
+        pic_out.write(img)
+
 
 for sn in six_num[1:]:
     if not os.path.isdir(f'./{sn}'):
@@ -19,20 +29,15 @@ for sn in six_num[1:]:
     print(sel[0].text)
 
     sel_tag = soup.select('div section span a')
-    print('total pages', sel_tag[-1].text)
-
-    ii = int(sel_tag[-1].text)
-    img_list = []
-    for i in range(ii):
-        rr = rq.get(nref+str(i+1)+'/')
-        soup = bs(rr.text, 'html.parser')
-        sel = soup.select("div a img")
-        img_src = sel[0]["src"]
-        img_get = rq.get(img_src)
-        img = img_get.content
-        with open(f'./{sn}/_{i+1}.png', 'wb') as pic_out:
-            pic_out.write(img)
-        img_list.append(f'./{sn}/_{i+1}.png')
-        print(f'Page {i+1} done!')
-    with open(f'./{sn}/{sn}.pdf', 'wb') as f:
-        f.write(img2pdf.convert(img_list))
+    print('Total pages', sel_tag[-1].text)
+    rr = rq.get(nref+'1/')
+    soup = bs(rr.text, 'html.parser')
+    sel = soup.select("div a img")
+    base_src = sel[0]["src"].split('1.png')[0]
+    img_src = [f'{base_src}{i}.png' for i in range(
+        1, int(sel_tag[-1].text))]
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(get_image, (img_src, sn))
+#    with open(f'./{sn}/{sn}.pdf', 'wb') as f:
+#        f.write(img2pdf.convert([f'./{sn}/_{i}.png' for i in range(
+#            1, int(sel_tag[-1].text))]))
