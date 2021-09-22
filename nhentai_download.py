847a8f8afdb55b11863b67fbb2a28ec7fa81604c
Jihong Lin <jihong860301@gmail.com>
2021-09-22 18:45:45 +0800

    fix some bugs in nhentai_overview.py, complete multi-thread function in nhentai_download.py

--- nhentai_download.py
+++ nhentai_download.py
@@ -5,39 +5,52 @@ import os
 import img2pdf
 from concurrent.futures import ThreadPoolExecutor
 
-##########################################
+# input six numbers for comics
 six_num = sys.argv[:]
-##########################################
 
 
-def get_image(img_src, sn):
+def get_image(img_src):
+    # download image
     img = rq.get(img_src, stream=True).content
     i = img_src.split('/')[5].split('.')[0]
-    print(f'./{sn}/_{i}.png')
-    with open(f'./{sn}/_{i}.png', 'wb') as pic_out:
+    with open(f'./_{i}.png', 'wb') as pic_out:
         pic_out.write(img)
 
 
 for sn in six_num[1:]:
+    # create folder for images
     if not os.path.isdir(f'./{sn}'):
         os.mkdir(sn)
+    # change dir to the created folder
+    os.chdir(f'./{sn}')
+    # base ref
     nref = f'https://nhentai.net/g/{sn}/'
 
+    # get and print title of the comic
     rr = rq.get(nref)
     soup = bs(rr.text, 'html.parser')
     sel = soup.select('div h2')
     print(sel[0].text)
 
+    # get and print the total pages of the comic
     sel_tag = soup.select('div section span a')
     print('Total pages', sel_tag[-1].text)
+
+    # get image urls
     rr = rq.get(nref+'1/')
     soup = bs(rr.text, 'html.parser')
     sel = soup.select("div a img")
-    base_src = sel[0]["src"].split('1.png')[0]
-    img_src = [f'{base_src}{i}.png' for i in range(
+    base_src = sel[0]["src"].split('1.jpg')[0]
+    img_src = [f'{base_src}{i}.jpg' for i in range(
         1, int(sel_tag[-1].text))]
+
+    # using multi-thread to download images
     with ThreadPoolExecutor(max_workers=10) as executor:
-        executor.map(get_image, (img_src, sn))
-#    with open(f'./{sn}/{sn}.pdf', 'wb') as f:
-#        f.write(img2pdf.convert([f'./{sn}/_{i}.png' for i in range(
-#            1, int(sel_tag[-1].text))]))
+        executor.map(get_image, img_src)
+
+    # make pdf with A-4 size
+    a4 = (img2pdf.mm_to_pt(210), img2pdf.mm_to_pt(297))
+    layout_fun = img2pdf.get_layout_fun(a4)
+    with open(f'./{sn}.pdf', 'wb') as f:
+        f.write(img2pdf.convert([f'./_{i}.png' for i in range(
+            1, int(sel_tag[-1].text))], layout_fun=layout_fun))
--- nhentai_overview.py
+++ nhentai_overview.py
@@ -1,26 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 
 class overview:
-    def __init__(self, key_word, pages='1', order='recent'):
-        self.key_word = key_word
+    def __init__(self, keyword='Default', pages='1', order='recent'):
+        self.keyword = keyword  # keyword
+        # pages: string ,can be one number or two number connect with '-', like'1-5'
         self.pages = pages
+        # order of searching result, 'recent', 'today', 'week', 'all time'
         self.order = order
 
-    def search(self):
-        if not os.path.isdir(f'./{self.key_word}_search'):
-            os.mkdir(self.key_word+'_search')
-        list_text = open(
-            f'./{self.key_word}_search/{self.key_word}_search.txt', 'w', encoding='UTF-8')
-        key_split = self.key_word.split()
+        # process keyword
+        key_split = self.keyword.split()
         if not len(key_split) == 1:
             search_keyword = ''
             for kk in key_split:
                 search_keyword = search_keyword+'+'+kk
-
+        else:
+            search_keyword = self.keyword
         nref = 'https://nhentai.net/search/?q='+search_keyword
+
+        # process order for url
         if self.order == 'today':
             nref = nref+'&sort=popular-today'
         elif self.order == 'week':
@@ -28,6 +30,22 @@ class overview:
         elif self.order == 'all time':
             nref = nref + '&sort=popular'
 
+        if search_keyword == 'Default':
+            nref = 'https://nhentai.net/'
+
+        # base url for searching result
+        self.url = nref
+
+    def search(self):
+        # create folder and change path to the folder
+        if not os.path.isdir(f'./{self.keyword}_search'):
+            os.mkdir(self.keyword+'_search')
+        os.chdir(f'./{self.keyword}_search')
+        # open a .txt file for comic list
+        list_text = open(
+            f'./{self.keyword}_search.txt', 'w', encoding='UTF-8')
+
+        # process pages for url
         if len(self.pages.split('-')) == 1:
             pp = [self.pages]
         else:
@@ -35,28 +53,30 @@ class overview:
             pp = [str(i) for i in range(int(ps[0]), int(ps[1])+1)]
 
         for p in pp:
-            nref1 = nref+'&page='+p
+            nref1 = self.url+'&page='+p
 
+            # get whole searching page info
             r = requests.get(nref1)
             soup = BeautifulSoup(r.text, "html.parser")
             sel = soup.select("div.gallery a")
 
             for s in sel:
+                # go into every comic to get info and download cover
                 nref2 = 'https://nhentai.net'+s["href"]
                 r2 = requests.get(nref2)
                 soup2 = BeautifulSoup(r2.text, 'html.parser')
                 sel2_title = soup2.select("div h2")
                 print(sel2_title[0].text)
+                # download cover
                 sel2_img = soup2.select("div a img")
                 sel_sixnum = soup2.select("div h3")
-
                 pic = requests.get(sel2_img[1]["src"])
                 img2 = pic.content
                 fn = s["href"].split('/')
-                pic_out = open('./'+self.key_word +
-                               '_search/#'+fn[2]+'.png', 'wb')
+                pic_out = open(f'./#{fn[2]}.png', 'wb')
                 pic_out.write(img2)
                 pic_out.close()
+                # write info into text, include title, tags and total pages
                 list_text.write(sel_sixnum[0].text +
                                 ':'+sel2_title[0].text+'\ttag:')
                 sel2_tags = soup2.select('div section span a')
@@ -67,3 +87,12 @@ class overview:
                 list_text.write(sel2_tags[-1].text+'p\n')
                 print(sel_sixnum[0].text, sel2_tags[-1].text+'p')
         list_text.close()
+
+    def set_keyword(self, keyword):
+        self.keyword = keyword  # set keyword
+
+    def set_pages(self, pages):
+        self.pages = pages  # set pages
+
+    def set_order(self, order):
+        self.order = order  # set order
