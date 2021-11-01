eb4ac4182cc53c96c2e783e123da14c057868323
Jihong Lin <jihong860301@gmail.com>
2021-11-01 17:48:25 +0800

    Resize preview image and add some commit

--- nhentaiDL_GUI.py
+++ nhentaiDL_GUI.py
@@ -3,17 +3,28 @@ from tkinter import messagebox
 from nhentai_download import nhentai_download
 from PIL import Image, ImageTk
 
-# THREAD = 16
+# 設定常數
 FONT = ('微軟正黑體', 16)
+WINDOW_SIZE = '1200x800'
+TITLE = 'nhentai download'
+# 如果下載路徑未設定，預設為與程式同資料夾
+# 如果六位數未設定，預設為按下預覽與下載皆不會有事情發生
+# 如果執行數未設定，預設為16
 
+# 初始化
 nhentai = nhentai_download()
-# init
+
 window = tk.Tk()
-window.title('nhentai download')
-window.geometry('1200x800')
+window.title(TITLE)
+window.geometry(WINDOW_SIZE)
 
 
 def preview_event():
+    # 預覽事件，按下預覽按鈕後發生的事情
+    # 1.下載本子的基本資料
+    # 2.顯示基本資料在GUI上
+    # 3.下載封面圖片
+    # 4.顯示在GUI右半側
     sixnum = sixnum_var.get()
     if sixnum == '':
         pass
@@ -41,15 +52,21 @@ def preview_event():
         lb_totalpages.grid(row=10, column=0, sticky='W', columnspan=3)
 
         cover_image = nhentai.show_cover()
-        img = ImageTk.PhotoImage(cover_image.resize((600, 800)))
+        cover_image = image_resize(cover_image)
+        y_pos = (800-cover_image.size[1])/2
+        img = ImageTk.PhotoImage(cover_image)
         image = tk.Canvas(window, width=600, height=800, bg='white')
         image.place(x=600, y=0)
-        image.create_image(0, 0, anchor=tk.NW, image=img)
+        image.create_image(0, y_pos, anchor=tk.NW, image=img)
 
         window.mainloop()
 
 
 def dlpdf_event():
+    # 下載PDF事件，在按下"下載PDF"後發生
+    # 六位數未設定的情況下不會有任何事發生
+    # 如果下載路徑與執行序數未設定皆為預設
+    # 下載完成跳出視窗提醒
     sixnum = sixnum_var.get()
     dlpath = dlpath_var.get()
     threads = thread_var.get()
@@ -64,6 +81,10 @@ def dlpdf_event():
 
 
 def dlimg_event():
+    # 下載圖片事件，在按下"下載圖片"後發生
+    # 六位數未設定的情況下不會有任何事發生
+    # 如果下載路徑與執行序數未設定皆為預設
+    # 下載完成跳出視窗提醒
     sixnum = sixnum_var.get()
     dlpath = dlpath_var.get()
     threads = thread_var.get()
@@ -78,10 +99,17 @@ def dlimg_event():
 
 
 def dlfinish_popwindow():
+    # 下載完成的提示視窗
     messagebox.showinfo('nhentai download', '下載完成!')
 
 
-# labels and entries
+def image_resize(img):
+    # 預覽圖片的大小重新塑形
+    img = img.resize((600, int(img.size[1]*(600/img.size[0]))))
+    return img
+
+
+# 基本的版面配置(Label與Entry)
 lb_path = tk.Label(window, text='下載路徑:',
                    font=FONT)
 lb_path.grid(row=0, column=0, sticky='W')
@@ -107,7 +135,7 @@ entry_thread = tk.Entry(window, textvariable=thread_var,
                         font=FONT)
 entry_thread.grid(row=2, column=1)
 
-# bottom
+# 按鈕配置
 button_preview = tk.Button(window, text='預覽', font=FONT, command=preview_event)
 button_preview.grid(row=3, column=0)
 
@@ -117,5 +145,6 @@ button_dlpdf.grid(row=3, column=1)
 
 button_dlimg = tk.Button(window, text='下載圖片', font=FONT, command=dlimg_event)
 button_dlimg.grid(row=3, column=2)
-# show
+
+# 顯示於GUI上
 window.mainloop()
